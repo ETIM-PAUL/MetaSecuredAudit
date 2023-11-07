e94c74f27ff821c913d822ca365cd2716cba2495
etim <etimpaul22@gmail.com>
2023-11-08 00:45:34 +0100

    added audits for Storage victim contract

---
+++ MetaSecure/README.md
@@ -0,0 +1,22 @@
+# Auditing Smart Contract
+
+A small dapp where organizations are created with stakeholders (who can be whitelisted to claim tokens after a period of time).
+
+## Description
+
+This is an audit report for a smart contract (Storage Victim) written in solidity.
+I found the follow bugs with the corresponding level of vulnerability
+i. uninitialised pointer - critical
+ii. old compiler version - medium
+iii. no license identifier - educational
+iv.  marking state variable as immutable - gas optimization(optional)
+v external functions - educational(gas optimization)
+
+## Author
+
+Etim Paul E.
+
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ MetaSecure/src/Secure.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract StorageVictim {
+    address immutable owner;
+
+    struct Storage {
+        address user;
+        uint256 amount;
+    }
+
+    mapping(address => Storage) storages;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function store(uint256 amount) external {
+        Storage memory str;
+
+        str.user = msg.sender;
+
+        str.amount = amount;
+
+        storages[msg.sender] = str;
+    }
+
+    function getStore() external view returns (address, uint256) {
+        Storage storage str = storages[msg.sender];
+
+        return (str.user, str.amount);
+    }
+
+    function getOwner() external view returns (address) {
+        return owner;
+    }
+}
